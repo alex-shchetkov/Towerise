0123ca71dfd0ea7109d8a355ab5344866e2330de
alex-shchetkov <alex.v.shch@gmail.com>
2017-10-28 22:43:17 -0400

    testing full build

--- Towerise/Controllers/SampleDataController.cs
+++ Towerise/Controllers/SampleDataController.cs
@@ -11,7 +11,7 @@ namespace Towerise.Controllers
     {
         private static string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Kappa", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         [HttpGet("[action]")]
