d80806a45f24cf520a7b9874a20404a46f59f535
Joshua Landi <landifam4@gmail.com>
2017-11-05 13:12:02 -0500

    importing

--- Towerise/ClientApp/app/components/player/player.component.ts
+++ Towerise/ClientApp/app/components/player/player.component.ts
@@ -1,35 +1,6 @@
 ﻿import { Component, AfterViewInit, HostListener } from '@angular/core';
 import { getBaseUrl } from "../../app.module.browser";
-
-class Vector2 {
-    public x: number;
-    public y: number;
-
-    constructor(x: number, y: number) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public get magnitude() {
-        return Math.sqrt((this.x * this.x) + (this.y * this.y));
-    }
-
-    public normalize() {
-        return new Vector2((this.x / (this.magnitude || 1)), (this.y / (this.magnitude || 1)));
-    }
-
-    public mult(multiplicationVector: Vector2) {
-        return new Vector2(this.x * multiplicationVector.x, this.y * multiplicationVector.y);
-    }
-
-    public add(additionVector: Vector2) {
-        return new Vector2(this.x + additionVector.x, this.y + additionVector.y);
-    }
-
-    public sub(subtractionVector: Vector2) {
-        return new Vector2(this.x - subtractionVector.x, this.y - subtractionVector.y);
-    }
-}
+import { Vector2 } from "../../shared/Vector2";
 
 @Component({
     selector: 'player',
---
+++ Towerise/ClientApp/app/shared/Vector2.ts
@@ -0,0 +1,29 @@
+﻿export class Vector2 {
+    public x: number;
+    public y: number;
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public get magnitude() {
+        return Math.sqrt((this.x * this.x) + (this.y * this.y));
+    }
+
+    public normalize() {
+        return new Vector2((this.x / (this.magnitude || 1)), (this.y / (this.magnitude || 1)));
+    }
+
+    public mult(multiplicationVector: Vector2) {
+        return new Vector2(this.x * multiplicationVector.x, this.y * multiplicationVector.y);
+    }
+
+    public add(additionVector: Vector2) {
+        return new Vector2(this.x + additionVector.x, this.y + additionVector.y);
+    }
+
+    public sub(subtractionVector: Vector2) {
+        return new Vector2(this.x - subtractionVector.x, this.y - subtractionVector.y);
+    }
+}
