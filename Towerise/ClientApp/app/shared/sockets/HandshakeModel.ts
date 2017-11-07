f618ae155798d7f496bb9294a96830d3746b968f
Joshua Landi <jlandi@ncsu.edu>
2017-11-07 17:18:27 -0500

    reorganizing.  removing.  still not happy, but its better.  opponents are kind of jumpy and at first login player is in upright corner for half a second (#7)

---
+++ Towerise/ClientApp/app/shared/sockets/HandshakeModel.ts
@@ -0,0 +1,10 @@
+﻿export class HandshakeModel {
+    Name: string;
+    constructor(name: string) {
+        this.Name = name;
+    }
+
+    public get stringifiedModel(): string {
+        return JSON.stringify({ Name: this.Name });
+    }
+}
